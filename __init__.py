6465c578997ad6e56cd1033a689427a7e6607faa
roykillington <superboss@roykillington.com>
2025-03-04 10:40:05 -0800

    Add __init__.py for initialization in Comfy

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .VeniceUpscale import VeniceUpscale
+
+NODE_CLASS_MAPPINGS = {
+    "VeniceUpscale": VeniceUpscale
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "VeniceUpscale": "Venice AI Upscale"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
